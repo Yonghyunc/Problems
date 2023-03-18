2c05e1df08624b1c8090e63b2d98b025215d11e0
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-03-18 18:10:18 +0900

    [SWEA] 1238. contact

---
+++ 스터디/1238_contact/p1.py
@@ -0,0 +1,35 @@
+# SWEA 1238. contact
+
+from collections import deque
+
+for tc in range(10):
+    l, start = map(int, input().split())
+    data = list(map(int, input().split()))
+
+    to = [[] for _ in range(101)]
+    visited = [0] * 101
+
+    for i in range(l // 2):
+        to[data[i * 2]].append(data[i * 2 + 1])
+
+    que = deque()
+    que.append([start, 1])
+    visited[start] = 1
+
+    result = 0
+    max_step = 0
+
+    while que:
+        num, step = que.popleft()
+        if max_step < step:
+            max_step = step
+            result = num
+        if max_step == step and result < num:
+            result = num
+
+        for n in to[num]:
+            if not visited[n]:
+                que.append([n, step + 1])
+                visited[n] = 1
+
+    print(f'#{tc + 1} {result}')
