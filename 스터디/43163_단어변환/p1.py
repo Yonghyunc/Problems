fdaa423a4b7ab9749b74a19448e3929ce26562b9
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-03-18 18:08:42 +0900

    [프로그래머스] 43163. 단어 변환

---
+++ 스터디/43163_단어변환/p1.py
@@ -0,0 +1,37 @@
+# 프로그래머스 43163. 단어변환
+
+from collections import deque
+
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+
+    word_cnt = len(words)
+    visited = [0] * word_cnt
+    word_len = len(begin)
+
+    used = deque()
+    used.append([begin, 0])
+    while used:
+        now, cnt = used.popleft()
+
+        if now == target:
+            return cnt
+
+        for i in range(word_cnt):
+            if not visited[i]:
+                diff = 0
+                for j in range(word_len):
+                    if now[j] != words[i][j]:
+                        diff += 1
+                else:
+                    if diff == 1:
+                        visited[i] = 1
+                        used.append([words[i], cnt + 1])
+    return 0
+
+
+
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]))
